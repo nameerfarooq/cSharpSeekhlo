069bfc6f4c11f9c83ee5cca209192e7bd97aedc6
nameerfarooq <nameerfarooquit@gmail.com>
2021-11-27 12:19:21 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+namespace DataTypesCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("you entered " + num1);
+        }
+    }
+}
