23bfbe7de4cf791e21fe03533a16a3944aa80660
nameerfarooq <nameerfarooquit@gmail.com>
2021-11-27 12:51:17 +0500

    arraysImplemented

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,12 +5,24 @@ namespace BASIC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("please enter a number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            for(int i = 1; i < 11; i++)
-            {
-                Console.WriteLine(num1 + "x" + i + "=" + num1*i);
-            }
+            //for loops
+
+            //Console.WriteLine("please enter a number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //for(int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine("{0}x{1}={2}",num1,i,num1*i);
+            //}
+
+            //Arrays
+
+            //string[] cars = { "corolla", "civic", "vigo", "cultus" };
+            //for (int i = 0; i < cars.Length; i++)
+            //{
+            //    Console.WriteLine(cars[i]);
+            //}
+
+
         }
     }
 }
