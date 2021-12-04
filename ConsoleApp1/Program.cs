c32b70aaaf0721c556492399e7a38774d4b9daa9
Muhammad Nameer <61945782+nameerfarooq@users.noreply.github.com>
2021-12-04 17:06:40 +0500

    Update Program.cs
    lab 0 added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,6 +21,103 @@ namespace BASIC
             //{
             //    Console.WriteLine(cars[i]);
             //}
+            
+            
+            //lab 0 
+
+            //question 1
+
+            //Console.WriteLine("enter 1st number");
+            //var a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter 2nd number");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter 3rd number");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter 4th number");
+            //int d = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter 5th number");
+            //int e = Convert.ToInt32(Console.ReadLine());
+            //int result = (a + b + c + d + e) / 5;
+            //Console.WriteLine("average of 5 numbers:"+result);
+
+
+            //question 2 
+
+            //Console.WriteLine("Circle");
+            //Console.WriteLine("Enter Radius of circle");
+            //double RadiusCircle = Convert.ToDouble(Console.ReadLine());
+
+            //double areaOfCircle = 3.14 * (RadiusCircle * RadiusCircle);
+            //Console.WriteLine("Area of circle is :" + areaOfCircle);
+
+            //Console.WriteLine("Triangle");
+            //Console.WriteLine("enter lenght of triangle : ");
+            //double l = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("enter breadth of triangle : ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Area of triangle is :" + l * b);
+
+
+            //question 3
+
+            //Console.WriteLine("Basic arithmetic operators");
+            //Console.WriteLine("enter a value : ");
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter b value : ");
+
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("result of A + B :" + (a + b));
+            //Console.WriteLine("result of A - B :" + (a - b));
+            //Console.WriteLine("result of A * B :" + (a * b));
+            //Console.WriteLine("result of A / B :" + (a / b));
+            //Console.WriteLine("result of A % B :" + (a % b));
+            //Console.WriteLine("result of A ++ :" + a++);
+            //Console.WriteLine("result of A -- :" + a--);
+
+            //q4 reversed string
+
+            //Console.WriteLine("enter a string to be reversed");
+            //string a = Console.ReadLine();
+            //string b = "";
+            //for (int i = a.Length -1 ; i>=0; i--)
+            //{
+            //    b = b + a[i];
+            //}
+            //Console.WriteLine("the reverse string is : "+ b);
+
+
+            //q5 if condition
+
+            //Console.WriteLine("even odd program");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //if (num % 2 == 0 )
+            //{
+            //    Console.WriteLine("enter number is even");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("enter number is odd");
+
+            //}
+
+            //q6 pattern
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = i; j >= 1; j--)
+            //    {
+            //        Console.Write(j + " ");
+            //    }
+            //    for (int k = 2; k <= (6-i); k++)
+            //    {
+            //        Console.Write(k + " ");                  
+            //    }
+            //    Console.WriteLine();
+            //}
+            
+            
 
 
         }
